2567e33f48ce8f90a414f938a92dd79c602240fb
deepdubey <43035512+deepdubey@users.noreply.github.com>
2019-09-08 15:47:00 +0530

    Delete coinModel.js

--- models/coinModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Coin = new Schema({
-    name: { type: String },
-    price: { type: Number }, 
-}, { collection: 'coins' });
-
-module.exports = mongoose.model('Coins', Coin);
